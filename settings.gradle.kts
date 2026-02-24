7ba792c318d4fbfdd4c37e848a796ca1e1d52a9b
isalvama <inesalvans@gmail.com>
2026-02-24 10:46:06 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "TascaS1.09.Enums"
---
+++ src/main/java/first_level/FirstLevelMain.java
@@ -0,0 +1,14 @@
+package first_level;
+
+/*
+Exercicis:
+Crea un enum anomenat Day amb els dies de la setmana. Fes una funció que rebi un Day i imprimeixi si és laborable o cap de setmana.
+Crea un enum Level amb valors LOW, MEDIUM i HIGH. Crea una classe Task amb una propietat Level i mostra com canvia el comportament en funció del nivell.
+Afegeix mètodes dins l’enum i comprova que poden tenir lògica (ex: getColor() per cada nivell de Level).
+Converteix un String a enum (amb valueOf) i gestiona errors si el valor no és vàlid.
+ */
+
+public class FirstLevelMain {
+    public static void main (String[] args){
+    }
+}
